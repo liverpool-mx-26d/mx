e1af68bb1dfdd1da381c1dbc474c24145652ce00
soporte-mx-n07 <mesequiel735@gmail.com>
2025-11-08 08:20:40 -0600

    Delete santander directory

--- santander/js/antiflicker.js
+++
@@ -1,7 +0,0 @@
-document.documentElement.style.opacity = "0";
-opacityHandle = function () {
-    document.documentElement.style.opacity = "1";
-    clearTimeout(timeout);
-}
-
-timeout = setTimeout(opacityHandle, 2000);
